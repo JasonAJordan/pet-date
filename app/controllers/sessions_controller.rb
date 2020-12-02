22bf15f3fe6b4c35d6d6e93c0374f10e072cf4a4
JasonAkiraJordan <jasonajordan0@gmail.com>
2020-12-02 11:54:55 -0500

    sessions inprogress

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,11 @@
 class SessionsController < ApplicationController
+
+    def logout 
+        cookies.delete(:user_id) 
+        redirect_to new_user_path
+    end 
+
+    def login 
+        #user = User.find_by(user_)
+    end 
 end
