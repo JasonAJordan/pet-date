51fba4c1fc28624fac9429e5f9878764b84ae7ea
JasonAkiraJordan <jasonajordan0@gmail.com>
2020-12-02 11:49:23 -0500

    routes sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+
   resources :locations
   resources :meetings
   resources :pets
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  delete "/sessions/logout", to: 'sessions#logout' as: 'logout'
+  delete "/sessions/logout", to: 'sessions#logout', as: 'logout'
 
   get "/sessions/new", to: 'sessions#new', as: 'new_login' 
   post '/sessions/login', to: 'sessions#login', as: 'login'
